8cc718ac8a45c427bd126c9d4e614a5bcc539125
Linux <michel.besedin@gmail.com>
2018-03-22 16:48:17 +0300

    testing os module

--- home_work_block6_Mike_B/HW_6_4.py
+++ home_work_block6_Mike_B/HW_6_4.py
@@ -1,4 +1,38 @@
 '''Задача 4
 Реализовать программу, которая выводит содержимое каталога в файловой системе.
 Путь к каталогу запрашивается у пользователя.
+
+
+import os
+dir = '/home/linux/GitHub/python130218_Bes/home_work_block6_Mike_B/breakfast'
+f = open('dirs.txt', 'w')
+for item in os.listdir():
+    if os.path.isdir(item):
+        print(item, file=f)
+f.close()
+'''
+import os
+dir = '/home/linux/GitHub/python130218_Bes/home_work_block6_Mike_B/breakfast'
+def walk(dir):
+  for item in os.listdir(dir):
+    path = os.path.join(dir, item)
+    if os.path.isfile(path):
+        print(item)
+    else:
+        walk(path)
+walk(dir)
+'''
+я думал выведет с конца как в задаче с цифрами, вопросик: а почему такая странная последовательность
+ вывода списка файлов в директории? 
+salo.txt
+curd_with_honey.txt
+borshch.txt
+with_toast.txt
+coffee.txt
+должно быть: 
+curd_with_honey.txt
+salo.txt
+borshch.txt
+with_toast.txt
+coffee.txt
 '''
---
+++ home_work_block6_Mike_B/test.py
@@ -0,0 +1,4 @@
+l = [1, 23, 55]
+
+d = {k: v for k,v in enumerate(l)}
+print(type(d))
