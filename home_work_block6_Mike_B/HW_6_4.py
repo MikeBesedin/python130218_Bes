60268e6dd376447f8fdda64c6efa3a330616c9f4
Linux <michel.besedin@gmail.com>
2018-03-23 22:54:15 +0300

    допилил HW_6_4

--- home_work_block6_Mike_B/HW_6_4.py
+++ home_work_block6_Mike_B/HW_6_4.py
@@ -1,18 +1,34 @@
 '''Задача 4
-Реализовать программу, которая выводит содержимое каталога в файловой системе.
+Реализовать программу, которая выводит содержимое каталога в файловой системе (директории и файлы?).
 Путь к каталогу запрашивается у пользователя.
-
-
-import os
-dir = '/home/linux/GitHub/python130218_Bes/home_work_block6_Mike_B/breakfast'
-f = open('dirs.txt', 'w')
-for item in os.listdir():
-    if os.path.isdir(item):
-        print(item, file=f)
-f.close()
 '''
+
+# у меня вышло 2 варианта решения, первый записывает в файл полностью содержание: директории и файлы
 import os
-dir = '/home/linux/GitHub/python130218_Bes/home_work_block6_Mike_B/breakfast'
+dir = input('Enter path to some dir which content you want write into dirs.txt: ')
+f = open('dirs.txt', 'w')
+def walk(dir):
+    for item in os.listdir(dir):
+        path = os.path.join(dir, item)
+        if os.path.isfile(path):
+            print(item, file=f)
+        else:
+            print(item, file=f)
+            walk(path)
+walk(dir)
+f.close()
+# i need help!
+# my path for example: /home/linux/GitHub/python130218_Bes/home_work_block6_Mike_B/breakfast
+# внутри breakfast лежит два файла coffee.txt и with_toast.txt и папка Lunch,
+# в которой лежат salo.txt и borshch.txt, а также директория Dinner,
+# внутри которой одинокий curd_with_honey.txt
+# Первым записывается в файл Lunch. Окей.
+# Но почему второй строкой идет не имя вложенной в Lunch директории  Dinner, а файл salo.txt? Вот как он затесался...
+# А уже потом идет Dinner и функция нормально разворачивается с конца дописывает файлы в предсказуемой последовательности
+'''
+# Вариант 2 принтует только название внутренних файлов:
+import os
+dir = input('Enter path to some dir which content you want write into dirs.txt: ')
 def walk(dir):
   for item in os.listdir(dir):
     path = os.path.join(dir, item)
@@ -21,18 +37,4 @@ def walk(dir):
     else:
         walk(path)
 walk(dir)
-'''
-я думал выведет с конца как в задаче с цифрами, вопросик: а почему такая странная последовательность
- вывода списка файлов в директории? 
-salo.txt
-curd_with_honey.txt
-borshch.txt
-with_toast.txt
-coffee.txt
-должно быть: 
-curd_with_honey.txt
-salo.txt
-borshch.txt
-with_toast.txt
-coffee.txt
-'''
+
--- home_work_block6_Mike_B/test.py
+++
@@ -1,4 +0,0 @@
-l = [1, 23, 55]
-
-d = {k: v for k,v in enumerate(l)}
-print(type(d))
